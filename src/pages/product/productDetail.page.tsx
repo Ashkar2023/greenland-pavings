560a44cea70aca07707fe444706c5c020ac64405
Abhijith <abhijithm050@gmail.com>
2025-03-04 15:12:09 +0530

    fix: Adjust layout positioning on product details page

--- src/pages/product/productDetail.page.tsx
+++ src/pages/product/productDetail.page.tsx
@@ -19,19 +19,23 @@ export const ProductDetailPage = () => {
   }
 
   return (
-    <div className="h-screen bg-secondary p-4">
-      <div className="grid grid-flow-row grid-cols-[1fr_1fr] min-h-full gap-4">
+    <div className="h-screen bg-white p-4">
+      <div className="grid grid-flow-row md:grid-cols-[1fr_1fr] min-h-full gap-4">
         {/* Left Side: Embla Carousel */}
-        <div className="bg-red-100 h-full w-full p-4 flex justify-center items-center">
-          <EmblaCarousel images={product.images} />
+        <div className=" h-full w-full  flex justify-center items-center">
+          <div className="w-full h-full">
+            <EmblaCarousel images={product.images} />
+          </div>
         </div>
-
+  
         {/* Right Side: Product Details */}
-        <div className="bg-red-300 h-full w-full p-6 flex flex-col justify-center">
+        <div className=" h-full w-full p-6 flex flex-col justify-center">
           <h1 className="text-3xl font-bold mb-4">{product.name}</h1>
           <p className="text-lg">{product.description}</p>
         </div>
       </div>
     </div>
   );
+  
+  
 };
