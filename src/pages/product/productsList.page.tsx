209c12cbeb39ad8386cdf96b3e38cec57834f6c7
Abhijith <abhijithm050@gmail.com>
2025-03-23 15:10:43 +0530

    feat: nothing

--- src/pages/product/productsList.page.tsx
+++ src/pages/product/productsList.page.tsx
@@ -8,20 +8,23 @@ export const ProductsListPage = () => {
 
     return (
         <div className="bg-accent w-full">
-            {/* Banner Section */}
-            <div className="w-full h-[300px] sm:h-[400px] lg:h-screen flex justify-center items-center bg-accent overflow-hidden">
-                <Image
-                    alt="Card background"
-                    className="w-full h-full object-cover"
-                    src="https://heroui.com/images/hero-card-complete.jpeg"
-                />
-            </div>
+        {/* Banner Section */}
+        <div className="w-screen bg-black h-[300px] sm:h-[400px] lg:h-[400px] mt-4 bg-accent overflow-hidden flex">
+            <Image
+                alt="Banner background"
+                className="w-full h-full object-cover"
+                src="https://heroui.com/images/hero-card-complete.jpeg"
+            />
+        </div>
+    
+
+    
 
             {/* Product Grid */}
             <div className="bg-accent w-full py-6 px-2 grid grid-cols-2 gap-2 sm:gap-4 md:grid-cols-2 lg:grid-cols-4">
                 {productData.slice(0, 4).map(product => (
                     <Card
-                        className="md:min-w-[270px] flex flex-col h-[450px] transition-all duration-300 border border-transparent hover:border-black hover:shadow-lg cursor-pointer"
+                        className="md:min-w-[270px] flex flex-col h-[450px] transition-all duration-300 border border-transparent hover:border-app-accent hover:shadow-lg cursor-pointer"
                         key={product.model}
                         shadow="sm"
                         radius="sm"
@@ -47,7 +50,7 @@ export const ProductsListPage = () => {
             </div>
 
             {/* Second Banner Section */}
-            <div className="w-full h-[300px] sm:h-[400px] lg:h-screen flex justify-center items-center bg-accent overflow-hidden">
+            <div className="w-full h-[300px] sm:h-[400px] lg:h-[400px] mt-4 flex justify-center items-center bg-accent overflow-hidden">
                 <Image
                     alt="Card background"
                     className="w-full h-full object-cover"
@@ -59,7 +62,7 @@ export const ProductsListPage = () => {
             <div className="bg-accent w-full py-6 px-2 grid grid-cols-2 gap-2 sm:gap-4 md:grid-cols-2 lg:grid-cols-4">
                 {productData.slice(4).map(product => (
                     <Card
-                        className="md:min-w-[270px] flex flex-col h-[450px] transition-all duration-300 border border-transparent hover:border-black hover:shadow-lg cursor-pointer"
+                        className="md:min-w-[270px] flex flex-col h-[450px] transition-all duration-300 border border-transparent hover:border-app-accent hover:shadow-lg cursor-pointer"
                         key={product.model}
                         shadow="sm"
                         radius="sm"
